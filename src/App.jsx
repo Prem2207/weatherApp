f0b0474ccef3e1f6fdedcc20b8e0f8008796de25
franco gabriel silva <silva1501995@gmail.com>
2022-10-31 09:07:17 -0300

    proyecto final app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import AppWeather from './components/AppWeather'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <AppWeather/>
+    </div>
+  )
+}
+
+export default App
